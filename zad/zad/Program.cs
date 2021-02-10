fe17993663a58d407e8fd86d2f01879e274259e4
PreskoPicha <presko1020@gmail.com>
2021-02-10 09:38:38 +0200

    add zadacha from 10.02

---
+++ zad/zad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kolko uchenici ima? ");
+            int n = int.Parse(Console.ReadLine());
+            double obshto;
+            double suma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Wuwedete uspeha: ");
+                double u = double.Parse(Console.ReadLine());
+                suma = suma + u;
+            }
+            obshto = suma / n;
+            Console.WriteLine("{0:f2}", obshto);
+        }
+    }
+}
