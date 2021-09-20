ebfd62c3a7f74957c77d0f6f1b2d666b57a9b7dc
PreskoPicha <78840758+PreskoPicha@users.noreply.github.com>
2021-09-20 10:25:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace broini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zapishete desetichno chislo: ");
+            int desetichno = int.Parse(Console.ReadLine());
+            string dvoichno = Convert.ToString(desetichno,2);
+            Console.WriteLine("dvoichno: " + dvoichno);
+        }
+    }
+}
